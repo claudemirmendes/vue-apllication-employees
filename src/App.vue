1272cc2fde151723a75c60f03a081ddc0c26911f
claudemir <claudemirmendes>
2019-11-22 17:54:59 -0300

    adicionando validação de form

--- src/App.vue
+++ src/App.vue
@@ -46,7 +46,7 @@ export default {
 
       this.employees = [...this.employees, newEmployee]
     }
-  }
+  },
 }
 </script>
 
--- src/components/EmployeeForm.vue
+++ src/components/EmployeeForm.vue
@@ -2,9 +2,25 @@
     <div id="employee-form">
       <form @submit.prevent="handleSubmit">
           <label>Employee name</label>
-          <input v-model="employee.name" type="text"/>
+          <input ref="first"
+                 :class="{'has-error': submitting && invalidName}"
+                 v-model="employee.name"
+                 @focus="clearStatus"
+                 @keypress="clearStatus"
+                 type="text"/>
           <label>Employee Email </label>
-          <input v-model="employee.email" type="text"/>
+          <input v-model="employee.email" 
+                 ref="first"
+                 type="text"
+                 :class="{'has-error': submitting && invalidEmail}"
+                 @focus="clearStatus"
+                 />
+                 <p v-if="error && submitting" class="eror-message">
+                   ❗Please fill out all required fields
+                 </p>
+                 <p v-if="success" class="success-mesage">
+                   ✅ Employee successfully added 
+                </p>
           <button> Add Employee </button>
       </form>
     </div>
@@ -15,6 +31,9 @@ export default {
     name: 'employee-form',
     data() {
         return {
+            submitting : false,
+            error: false,
+            success: false, 
             employee:{
                 name: '',
                 email: '',
@@ -23,14 +42,50 @@ export default {
     },
     methods: {
         handleSubmit(){
+          this.submitting  = true
+          this.clearStatus()
+          if (this.invalidName || this.invalidEmail) {
+              this.error = true
+              return
+          }
           this.$emit('add:employee', this.employee)
+          this.employee = {
+              name: '',
+              email: ''
+          }
+          this.error = false
+          this.success = true
+          this.submitting = false
         },
+        clearStatus() {
+            this.success = false
+            this.error = false
+        }
     },
+    computed :{
+      invalidName() {
+        return this.employee.name === '';
+    },
+    invalidEmail() {
+      return this.employee.email === '';
+    }
+  }
 }
 </script>
 <style scoped>
   form {
       margin-bottom: 2rem;
+  }
 
+  [class*='-message']{
+      font-weight: 500;
+  }
+
+  .error-message {
+      color: #d33c40;
+  }
+
+  .success-message {
+      color: #32a95d;
   }
 </style>
